8b37ec1aabc8970271686c20579d26f76e553829
Modern-Icarus <raziel.t99@gmail.com>
2026-03-04 12:24:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello LoreBot");
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
